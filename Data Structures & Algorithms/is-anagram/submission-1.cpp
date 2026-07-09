e175357a1618b8d7c02819f89919d6b5a7af04e7
Nayan Ravindra Khandelwal <nayankhandelwal261@gmail.com>
2026-07-09 17:33:54 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>s1;
+        for(int i=0; i<s.size(); i++){
+            s1[s[i]]++;
+        }
+
+        unordered_map<char,int>t1;
+        for(int j=0; j<t.size(); j++){
+            t1[t[j]]++;
+        }
+
+        if(s1 == t1){
+            return true;
+        }
+        return false;
+    }
+};
