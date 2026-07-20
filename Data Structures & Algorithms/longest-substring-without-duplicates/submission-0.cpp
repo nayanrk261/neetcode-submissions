3de18fe186fd9f64f35ce37bafb026d8bbc09878
Nayan Ravindra Khandelwal <nayankhandelwal261@gmail.com>
2026-07-20 18:09:13 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int>freq;
+        int left = 0;
+        int maxLen = 0;
+        for(int right=0; right<s.size(); right++){
+            if(freq.count(s[right])){
+                left = max(left, freq[s[right]]+1);
+            }
+            freq[s[right]] = right;
+            maxLen = max(maxLen, right-left+1);
+        }
+        return maxLen;
+    }
+};
