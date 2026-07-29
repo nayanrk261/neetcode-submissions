ca3f5e3038a1950a2624792d839690b889db31bd
Nayan Ravindra Khandelwal <nayankhandelwal261@gmail.com>
2026-07-29 20:50:10 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        unordered_map<char,char>match = {{'}','{'},{']','['},{')','('}};
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '{' || s[i] == '[' || s[i] == '('){
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty() &&    match[s[i]] == st.top()){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
