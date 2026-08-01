4608bdbd25ee78c526f34d8c9f62b35875c5975e
Nayan Ravindra Khandelwal <nayankhandelwal261@gmail.com>
2026-08-01 10:12:37 +0530

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.cpp
@@ -0,0 +1,41 @@
+class Solution {
+   public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        int result;
+
+        for (int i = 0; i < tokens.size(); i++) {
+            string t = tokens[i];
+
+            if (isdigit(t[0]) || (t.size()>1 && t[0] == '-' && isdigit(t[1]))) {
+                st.push(stoi(t));
+            } else {
+                if (st.size() < 2) {
+                    return -1;
+                }
+                int x = st.top();
+                st.pop();
+                int y = st.top();
+                st.pop();
+
+                if (t[0] == '+') {
+                    result = y + x;
+                } else if (t[0] == '-') {
+                    result = y - x;
+                } else if (t[0] == '*') {
+                    result = y * x;
+                } else {
+                    if (x == 0) {
+                        return -1; 
+                    }
+                    result = y / x;
+                }
+                st.push(result);
+            }
+        }
+        if (st.empty()) {
+            return -1;
+        }
+        return st.top();
+    }
+};
