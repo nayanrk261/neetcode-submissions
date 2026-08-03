0fa0b92bd98f05a38b7ba24f57a8b6a447ae185a
Nayan Ravindra Khandelwal <nayankhandelwal261@gmail.com>
2026-08-03 16:13:29 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int>ans(temperatures.size(),0);
+
+        for(int i=0; i<temperatures.size(); i++){
+            for(int j=i+1; j<temperatures.size(); j++){
+                if(temperatures[j] > temperatures[i]){
+                    int val = j-i;
+                    ans[i] = val;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
