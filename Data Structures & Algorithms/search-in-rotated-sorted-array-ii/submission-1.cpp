40e66dfdac20f0bc96f02a012a0fcdef8b4b6761
Nayan Khandelwal <nayankhandelwal261@gmail.com>
2026-09-11 23:56:13 +0530

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+   public:
+    bool search(vector<int>& nums, int target) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
